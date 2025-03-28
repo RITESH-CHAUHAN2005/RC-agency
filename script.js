5d1cdd7af36438d276311100278293591b402667
Ritesh Chauhan <riteshchauhan43792@gmail.com>
2025-03-29 03:26:40 +0530

    Add the files

---
+++ script.js
@@ -0,0 +1,19 @@
+const counters = document.querySelectorAll('.count');
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+
+        const increment = target / 200; // Speed of the counter
+
+        if (count < target) {
+            counter.innerText = Math.ceil(count + increment);
+            setTimeout(updateCount, 10);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    updateCount();
+});
+
